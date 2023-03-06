d13ffab1be4a618138a044d2ebb87f8308cce1f8
Xisca04 <xiscacerdavicens@gmail.com>
2023-03-06 10:44:19 +0100

    inicio_ej1

---
+++ Assets/Scripts/Exercise_1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise_1 : MonoBehaviour
+{
+    // Multiples cubes with random scales in ramdom positions
+
+    public Vector3[] randomScales;
+    public Vector3[] randomPositions;
+}
